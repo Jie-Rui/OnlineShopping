873d71c4657424994b84bd04905563506a17f5c2
Jie-Rui <2710539183@qq.com>
2022-09-15 11:23:17 +0800

    commit MyLogin

--- online-store/src/App.vue
+++ online-store/src/App.vue
@@ -5,11 +5,22 @@
 			<div class="top-bar">
 				<div class="nav">
 					<ul>
-						<li>
-							<el-button type="text">登录</el-button>
+						<li v-if="!this.$store.getters.getUser">
+							<el-button type="text" @click="login">登录</el-button>
 							<span class="sep">|</span>
 							<el-button type="text">注册</el-button>
 						</li>
+						<li v-else>
+              欢迎
+              <el-popover placement="top" width="180" v-model="visible">
+                <p>确定退出登录吗？</p>
+                <div style="text-align: right; margin: 10px 0 0">
+                  <el-button size="mini" type="text" @click="visible = false">取消</el-button>
+                  <el-button type="primary" size="mini" @click="logout">确定</el-button>
+                </div>
+                <el-button type="text" slot="reference">{{this.$store.getters.getUser.userName}}</el-button>
+              </el-popover>
+            </li>
 						<li>
 							<router-link to="order">我的订单</router-link>
 						</li>
@@ -45,11 +56,14 @@
 					<el-menu-item index="/about">关于我们</el-menu-item>
 					<div class="search">
 						<el-input placeholder="请输入搜索内容" v-model="search">
-							<el-button slot="append" icon="el-icon-search"></el-button>
+							<el-button slot="append" icon="el-icon-search" @click="searchClick"></el-button>
 						</el-input>
 					</div>
 				</el-menu>
 			</el-header>
+
+			<!-- 登录模块 -->
+      <MyLogin></MyLogin>
 
       <!-- 商品内容区域 -->
 			<el-main>
@@ -95,14 +109,79 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+import { mapGetters } from 'vuex';
+	
 export default {
 	data() {
 		return {
 			activeIndex: "", // 头部导航栏选中的标签
 			search: "", 
-      getNum: 0
+			visible: false,
 		};
 	},
+	beforeUpdate() {
+		this.activeIndex = this.$route.path
+	},
+	created() {
+    // 获取浏览器localStorage，判断用户是否已经登录
+    if (localStorage.getItem("user")) {
+      // 如果已经登录，设置vuex登录状态
+      this.setUser(JSON.parse(localStorage.getItem("user"))); 
+    }
+	},
+	computed: {
+		...mapGetters(['getUser', 'getNum']),
+		// 获取vuex的登录状态
+    getUser: function(val) {
+      if (val === "") {
+        // 用户没有登录
+        this.setShoppingCart([]);
+      } else {
+        // 用户已经登录,获取该用户的购物车信息
+        this.$http
+          .post("/user/shoppingCart/getShoppingCart/", {
+            user_id: val.user_id
+          })
+          .then(res => {
+            if (res.data.code === "001") {
+              // 001 为成功, 更新vuex购物车状态
+              this.setShoppingCart(res.data.shoppingCartData);
+            } else {
+              // 提示失败信息
+              this.notifyError(res.data.msg);
+            }
+          })
+          .catch(err => {
+            return Promise.reject(err);
+          });
+      }
+    },
+	},
+	methods: {
+		...mapActions(["setUser", "setShowLogin", "setShoppingCart"]),
+    login() {
+      // 点击登录按钮, 通过更改vuex的showLogin值显示登录组件
+      this.setShowLogin(true);
+    },
+		// 退出登录
+    logout() {
+      this.visible = false;
+      // 清空本地登录信息
+      localStorage.setItem("user", "");
+      // 清空vuex登录信息
+      this.setUser("");
+      this.notifySucceed("成功退出登录");
+    },
+		// 点击搜索按钮
+    searchClick() {
+      if (this.search != "") {
+        // 跳转到全部商品页面,并传递搜索条件
+        this.$router.push({ path: "/goods", query: { search: this.search } });
+        this.search = "";
+      }
+    }
+	}
 };
 </script>
 
